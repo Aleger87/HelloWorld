ca941c7a94f2c1b2fe8e94be79e73635d830dadd
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-08-08 21:43:19 +0300

    Добавлено условие if

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //переменные
         int a = 4;
         int b = 6;
         System.out.println(a + b);
-
+        // условные операторы
         if (a > b){
             System.out.println("a > b");
         }else if(a < b){
@@ -12,5 +13,10 @@ public class Main {
         }else{
             System.out.println("a = b");
         }
+        //циклы
+        for (int i = a; i <= b; i++){
+            System.out.println("i = " + i);
+        }
+
     }
 }
